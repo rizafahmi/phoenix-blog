5171daaabd22d4d6da589634ce2b78e3150e255a
Riza Fahmi <rizafahmi@gmail.com>
2016-04-30 10:34:51 +0700

    Add Post module

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule Blog.Router do
   scope "/", Blog do
     pipe_through :browser # Use the default browser stack
 
+    resources "/posts", PostController
     get "/", PageController, :index
   end
 
